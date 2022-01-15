d60eb4df7ab3ba615b91723863ddf55b8b51fff7
kanchan.pal <kanch786@gmail.com>
2022-01-16 01:08:59 +0530

    Add android native tabs configuration

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,4 +1,4 @@
-import React from "react-native";
+import React, { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator} from "react-navigation-tabs";
@@ -8,6 +8,7 @@ import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealsDetailsScreen from "../screens/MealsDetailsScreen";
 import FavoriteScreen from "../screens/FavoriteScreen";
+import {createMaterialBottomTabNavigator} from "react-navigation-material-bottom-tabs"
 
 const MealsNavigator = createStackNavigator(
     {
@@ -40,14 +41,15 @@ const MealsNavigator = createStackNavigator(
     }
 );
 
-const MealsFavTabNavigator = createBottomTabNavigator({
-        Meals: {
+const tabScreenConfig = {
+     Meals: {
             screen: MealsNavigator,
             navigationOptions: {
                 tabBarIcon: (tabInfo) => {
                     return <Ionicons name ='ios-restaurant'
                     size = {25} color = {tabInfo.tintColor}/>;
-                }
+                },
+                 tabBarColor: Colors.primaryColor
             }
         },
         Favorites: {
@@ -56,10 +58,21 @@ const MealsFavTabNavigator = createBottomTabNavigator({
                 tabBarIcon: (tabInfo) => {
                     return <Ionicons name ='ios-star'
                     size = {25} color = {tabInfo.tintColor}/>;
-                }
+                },
+                tabBarColor: Colors.accentColor
             }
         }
-    }, {
+    };
+
+
+const MealsFavTabNavigator = Platform.OS === 'android' ? createMaterialBottomTabNavigator(
+    tabScreenConfig, {
+        activeTintColor: 'white',
+        shifting : true
+    }
+):
+createBottomTabNavigator({
+        tabScreenConfig,
         tabBarOptions: {
             activeTintColor: Colors.accentColor
         }
